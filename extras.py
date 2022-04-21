113a7a0398762a41cebb81ac60ea2b614c1940ed
ThomasWeis <thomas.weisheidinger@gmx.at>
2022-04-21 09:09:10 +0200

    Create kekse.py, fixed typos, added some items
    Moved kekse to a separate file,
    Fixed some typos in food and drinks lists,
    Added some ideas to the '.give' lists

--- extras.py
+++ extras.py
@@ -1,6 +1,6 @@
 giveextras=['eine Rolle Toilettenpapier', 'eine Schmerztablette', 'eine Packung Girlanden',
             '...äh serviert ist das falsche Wort, gibt sollte das heißen, eine Decke.',
             'eine Tafel Schokolade', 'eine Tüte Chips', 'eine Wärmflasche in Teddy Bezug',
-            'ein Gartenteich voll Kraft', 'ein Fass Kraft', 'eine Tonne Kraft',
-            'einen knuddelverrückten Kraft-Glücksbringer','ein Kubikmeter Kraft',
-            'viel Kraft... sehr viel', 'eine Packung bunte Taschentücher']
+            'einen Gartenteich voll Kraft', 'ein Fass Kraft', 'eine Tonne Kraft',
+            'einen knuddelverrückten Kraft-Glücksbringer','einen Kubikmeter Kraft',
+            'viel Kraft... sehr viel', 'eine Packung bunte Taschentücher', 'einen Eimer gute Laune']
---
+++ kekse.py
@@ -0,0 +1,10 @@
+kekseGoodOnes = ['einen Schokoladenkeks', 'einen Vanillekeks', 'einen Doppelkeks',
+         'einen Keks', 'einen Erdbeerkeks', 'einen Schokoladen-Cheesecake-Keks',
+         'einen Glückskeks', 'einen riesigen Double Chocolate Chip Cookie',
+         'einen Butterkeks', 'einen Keks mit Pistaziencreme']
+
+kekse = ['einen Schokoladenkeks', 'einen Vanillekeks', 'einen Doppelkeks', 'keinen Keks',
+         'einen Keks', 'einen Erdbeerkeks', 'einen Schokoladen-Cheesecake-Keks',
+         'einen Glückskeks', 'einen Scherzkeks', 'einen Unglückskeks',
+         'einen riesigen Double Chocolate Chip Cookie', 'einen unsichtbaren Keks',
+         'einen Browser Cookie', 'einen Butterkeks', 'einen Keks mit Pistaziencreme']
