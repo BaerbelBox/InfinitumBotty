af96c01acd20fb53dd2cac0449c09195ff52cad2
Baerbel Box <bbox@autistenchat.org>
2024-07-11 07:46:51 +0200

    mtell command aded

--- FaustBot/Modules/TellObserver.py
+++ FaustBot/Modules/TellObserver.py
@@ -17,6 +17,6 @@ class TellObserver(PrivMsgObserverPrototype):
         if data['message'].find('.tell') != -1 and self._is_idented_mod(data, connection):
             connection.send_channel(data['messageCaseSensitive'][6:])
         if data['message'].find('.mtell') != -1 and self._is_idented_mod(data, connection):
-            connection.send_channel('\001ACTION'+data['messageCaseSensitive'][7:]+'\001')
+            connection.send_channel('\001ACTION '+data['messageCaseSensitive'][7:]+'\001')
     def _is_idented_mod(self, data: dict, connection: Connection):
         return data['nick'] in self._config.mods and connection.is_idented(data['nick'])
