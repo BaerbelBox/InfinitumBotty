bb948be648af5102bf77a959c026bec0b9c7ec27
Baerbel Box <bbox@autistenchat.org>
2024-07-11 07:42:58 +0200

    added 20 seconds delay to first greeters first greeting

--- FaustBot/Modules/TellObserver.py
+++ FaustBot/Modules/TellObserver.py
@@ -16,5 +16,7 @@ class TellObserver(PrivMsgObserverPrototype):
     def update_on_priv_msg(self, data, connection: Connection):
         if data['message'].find('.tell') != -1 and self._is_idented_mod(data, connection):
             connection.send_channel(data['messageCaseSensitive'][6:])
+        if data['message'].find('.mtell') != -1 and self._is_idented_mod(data, connection):
+            connection.send_channel('\001ACTION'+data['messageCaseSensitive'][7:]+'\001')
     def _is_idented_mod(self, data: dict, connection: Connection):
         return data['nick'] in self._config.mods and connection.is_idented(data['nick'])
